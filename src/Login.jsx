cfe1f65ca02e140ce4d67e5872b207c191e6326c
Farhan Aziz <mraziz459@gmail.com>
2024-08-08 20:59:00 +0500

    commited changes

--- src/Login.jsx
+++ src/Login.jsx
@@ -46,7 +46,7 @@ function Login() {
                 <input onChange={(event) => setPswd(event.target.value)} type="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
               </div>
               <button onClick={handleLogin} className="w-full text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Log in</button>
-              <p>Create your account? <Link to="/Signup">Signup</Link></p>
+              {/* <p>Create your account? <Link to="/Signup">Signup</Link></p> */}
             </form>
           </div>
         </div>
